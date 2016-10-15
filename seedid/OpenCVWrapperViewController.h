2a2188469037f0daf47425d7739869c263833231
TomHamer <tomhamer@live.com>
2016-10-15 19:29:53 +1100

    threshhold

---
+++ seedid/OpenCVWrapperViewController.h
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapperViewController.h
+//  seedid
+//
+//  Created by Thomas Hamer on 15/10/2016.
+//  Copyright © 2016 Thomas Hamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenCVWrapperViewController : UIViewController
+
+@end
