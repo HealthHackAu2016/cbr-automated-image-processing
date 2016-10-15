9191518a1a53ddd9c15b47659d1cf8b8915b8beb
Allen <allenhuang307@gmail.com>
2016-10-15 15:49:24 +1100

    length of pixel

--- image_preparer.py
+++ image_preparer.py
@@ -87,21 +87,11 @@ def find_brightest_spot(img):
     print(minVal)
 
 
-def rescale(img, fixed_size):
-    width, height, channels = img.shape
-
-    # gets image and size of current image
-    rectangle = crop_colours(img)
-    widthr, heightr, channelsr = rectangle.shape
-
-    # can be changed, fixes size of small rectangle
-    r = fixed_size / widthr
-    dim = (fixed_size, int(height * r))
-
-    # perform the actual resizing of the image
-    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-    return resized
-
+# returns in mm
+def length_per_pixel(rectangle_image):
+    # length is given to be 3 inches
+    length_of_pixel = (3 * 25.4) / rectangle_image.shape[0]
+    return length_of_pixel
 
 def brighten(img, value):
     # convert to hsv
@@ -111,3 +101,4 @@ def brighten(img, value):
     # reconvert to image
     img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     return img
+
